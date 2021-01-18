441dc603768291e0cf9c81c4f7398396b965e8d5
Samuel Tay <samueltayishere@gmail.com>
2021-01-18 21:58:09 +0800

    Upload config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+
+#!/usr/bin/env python
+
+data = {'telegram_api_id' : '123456',
+         'telegram_api_hash' : '12345678901234567890123456789012',
+         'telegram_bot_token' : '1234567890:abcdefghijklmnopqrstuvwxyzABCDEGHI',
+         'admin':'12345678',
+         } 
